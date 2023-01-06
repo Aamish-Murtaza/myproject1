d28df2447337378b40c0c4e38b85e79641384506
Aamish-Murtaza <aamish.murtaza18999@gmail.com>
2023-01-06 19:08:53 +0530

    add two nmbers

---
+++ code1.py
@@ -0,0 +1,12 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
